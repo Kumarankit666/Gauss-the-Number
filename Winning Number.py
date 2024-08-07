5a3d90992847786d0e66bb0731dc1d5da89ea17e
Ankit kumar <150576490+Kumarankit666@users.noreply.github.com>
2024-08-07 10:06:41 +0530

    Update Winning Number.py

--- Winning Number.py
+++ Winning Number.py
@@ -2,7 +2,9 @@ winning_Number = 45
 gaussed_Number = int(input('Please Enter a number between 1 to 100:- '))
 if winning_Number == gaussed_Number:
     print('Congratulatins You win \U0001F642')
+else:
+    print('Please Gauss correct number')
 if winning_Number < gaussed_Number:
     print('Oh you gaussed high')
-else:
+if winning_Number > gaussed_Number:
     print('Oh you gaussed low')
